4267cc2e5ef4b83a0ca34199e500e31c55803090
shizkun <pivotanimator95@gmail.com>
2016-07-22 14:23:02 +0800

    Done routing tutorial

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,23 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+
 import { AppComponent } from './app.component';
-bootstrap(AppComponent);
+import { appRouterProviders } from './app.routes';
+
+
+import { HTTP_PROVIDERS, XHRBackend} from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
+
+bootstrap(AppComponent, [
+    appRouterProviders,
+    HTTP_PROVIDERS,
+    {
+        provide: XHRBackend,
+        useClass: InMemoryBackendService
+    },
+    {
+        provide: SEED_DATA,
+        useClass: InMemoryDataService
+    }
+]);
---
+++ app/services/hero.service.ts
@@ -0,0 +1,31 @@
+import { Hero } from './../models/hero/hero';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { HEROES } from './mocks/mock-heroes';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class HeroService {
+    private heroesUrl = '/app/heroes';
+    constructor(private http: Http) {
+
+    }
+    getHeroes() {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+    private handleError(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
+
+    getHero(id: number) {
+        return this.getHeroes().then(heroes =>
+            heroes.find(hero =>
+                hero.id == id
+            )
+        );
+    }
+}
